0a897e4dce2957c0f6dc0ea26b19e5d2cb38c13a
Lucas Mansoldo <lucasmansoldo@Lucass-Mac-mini.local>
2023-05-24 10:17:47 -0300

    renaming

--- src/widgets/svelte-sample-app/tailwind.config.cjs
+++ src/widgets/svelte-sample-app/tailwind.config.cjs
@@ -1,9 +1,9 @@
-const heliosPreset = require('./src/ui/styles/presets/helios-preset.cjs')
+const violetPreset = require('./src/ui/styles/presets/violet-ui-preset.cjs')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.html', './src/**/*.svelte', ...heliosPreset.content],
-	presets: [heliosPreset],
+	content: ['./src/**/*.html', './src/**/*.svelte', ...violetPreset.content],
+	presets: [violetPreset],
 	theme: {
 		extend: {}
 	},
